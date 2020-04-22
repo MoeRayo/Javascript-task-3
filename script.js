2ed2e327cbd3ad6983e7d91cfcde4a9a3891fe7f
moe_rayo <48064606+MoeRayo@users.noreply.github.com>
2020-04-22 17:50:25 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 // a function that takes in a parameter (a whole number/integer) and checks if numbers from 1 to the given parameter (inclusive) are divisible by 2,3,5 while replacing with the appropriate words.
-function whole(number){
-    if(typeof number === 'number'){
+function whole(num){
+    if(typeof num === 'number'){
         // creates an array
         let arr=[];
-            for(let i = 1; i<=number; i++){
+            for(let i = 1; i<=num; i++){
                     if (i%2 === 0 && i%3 === 0 && i%5 === 0){
                         arr.push('yu-gi-oh');
                     } else if(i%2 === 0 && i%3 === 0){
@@ -34,4 +34,3 @@ function whole(number){
 
 //   passes a random number in the parameter
   console.log(whole(50));
-  
